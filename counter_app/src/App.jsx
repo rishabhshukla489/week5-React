fd33ae8be01eefe7f2c018d86d516b56f14c2239
Rishabh Shukla <rishabh.489.c@gmail.com>
2024-05-31 14:05:12 +0530

    usestate 2

--- counter_app/src/App.jsx
+++ counter_app/src/App.jsx
@@ -4,13 +4,20 @@ import { useState } from "react";
 
 function App(){
   const [count,setCount]=useState(0);
-  function onClickHandler(){
-    setCount(count+1)
-  };
+  
   return (
     <div>
-      <button onClick={onClickHandler}>{count}</button>
+      <CustomButton count={count} setCount={setCount}></CustomButton>
     </div>
   )
+}
+
+function CustomButton(props){
+  function onClickHandler(){
+    props.setCount(props.count+1)
+  };
+  return <button onClick={onClickHandler}>
+    {props.count}
+  </button>
 }
 export default App
